07d90b72bf1e82253e6ae88d144f321d7437d52f
Naomi Yocum <naomiyocum24@gmail.com>
2022-10-10 20:08:34 -0600

    feat: user story 1, parent index

--- spec/features/crews/index_spec.rb
+++ spec/features/crews/index_spec.rb
@@ -3,9 +3,11 @@ require 'rails_helper'
 RSpec.describe 'index of crews' do
   it 'shows the name of each crew' do
     crew = Crew.create(name: 'Straw Hat Pirates', ranking: 1, ship: 'Thousand Sunny', worst_generation: true)
+    crew_2 = Crew.create(name: 'Roger Pirates', ranking: 2, ship: 'Oro Jackson', worst_generation: false)
     
     visit '/crews'
-
-    expect(page).to have_content(crew)
+    
+    expect(page).to have_content(crew.name)
+    expect(page).to have_content(crew_2.name)
   end
 end
