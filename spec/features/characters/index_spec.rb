ea951f372683bcf282fcb8e5412714f88b13ae62
Naomi Yocum <naomiyocum24@gmail.com>
2022-10-10 21:03:01 -0600

    fix styling for multiple characters

--- spec/features/characters/index_spec.rb
+++ spec/features/characters/index_spec.rb
@@ -4,26 +4,42 @@ RSpec.describe 'index of characters' do
   it 'shows the name of each character' do
     crew = Crew.create!(name: 'Straw Hat Pirates', ranking: 1, ship: 'Thousand Sunny', worst_generation: true)
     luffy = Character.create!(name: 'Monkey D. Luffy', age: 19, devil_fruit_eater: true, epithet: 'Straw Hat', bounty: 30_000_000, crew_id: crew.id)
-    
+    chopper = Character.create!(name: 'Tony Tony Chopper', age: 17, devil_fruit_eater: true, epithet: 'Cotton Candy Lover', bounty: 10, crew_id: crew.id)
     visit '/characters'
     
     expect(page).to have_content(luffy.name)
+    expect(page).to have_content(chopper.name)
   end
 
   it 'shows each characters attributres' do
     crew = Crew.create!(name: 'Straw Hat Pirates', ranking: 1, ship: 'Thousand Sunny', worst_generation: true)
     luffy = Character.create!(name: 'Monkey D. Luffy', age: 19, devil_fruit_eater: true, epithet: 'Straw Hat', bounty: 30_000_000, crew_id: crew.id)
-    
+    chopper = Character.create!(name: 'Tony Tony Chopper', age: 17, devil_fruit_eater: true, epithet: 'Cotton Candy Lover', bounty: 10, crew_id: crew.id)
+
     visit '/characters'
     
     expect(page).to have_content(luffy.id)
+    expect(page).to have_content(chopper.id)
+
     expect(page).to have_content(luffy.age)
+    expect(page).to have_content(chopper.age)
+
     expect(page).to have_content(luffy.devil_fruit_eater)
+    expect(page).to have_content(chopper.devil_fruit_eater)
+
     expect(page).to have_content(luffy.epithet)
+    expect(page).to have_content(chopper.epithet)
+
     expect(page).to have_content(luffy.bounty)
+    expect(page).to have_content(chopper.bounty)
+
     expect(page).to have_content(luffy.crew_id)
+    expect(page).to have_content(chopper.crew_id)
 
     expect(page).to have_content(luffy.created_at)
+    expect(page).to have_content(chopper.created_at)
+
     expect(page).to have_content(luffy.updated_at)
+    expect(page).to have_content(chopper.updated_at)
   end
 end
