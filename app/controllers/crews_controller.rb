9ff0747d743a63fc0350c7bf1bc735c0bc22a787
Naomi Yocum <naomiyocum24@gmail.com>
2022-10-12 16:03:53 -0600

    crew update - us12

--- app/controllers/crews_controller.rb
+++ app/controllers/crews_controller.rb
@@ -10,11 +10,22 @@ class CrewsController < ApplicationController
   def new
   end
 
+  def edit
+    @crew = Crew.find(params[:id])
+  end
+
+  def update
+    crew = Crew.find(params[:id])
+    crew.update(crew_params)
+    redirect_to "/crews/#{crew.id}"
+  end
+
   def create
     crew = Crew.create(crew_params)
     redirect_to "/crews"
   end
 
+private
   def crew_params
     params.permit(:name, :ship, :ranking, :worst_generation)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get '/crews', to:'crews#index'
   get '/crews/new', to: 'crews#new'
   get '/crews/:id', to:'crews#show'
+  get '/crews/:id/edit', to:'crews#edit'
   
   get '/crews/:id/characters', to:'crew_characters#index'
 
@@ -9,4 +10,5 @@ Rails.application.routes.draw do
   get '/characters/:id', to:'characters#show'
 
   post '/crews', to:'crews#create'
+  patch '/crews/:id', to: 'crews#update'
 end
