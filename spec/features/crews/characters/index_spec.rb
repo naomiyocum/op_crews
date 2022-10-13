6ccfefa80b5dbb6737ea5d80d11494d5688052c5
Naomi Yocum <naomiyocum24@gmail.com>
2022-10-13 13:57:49 -0600

    delete button every mem index pg - us23

--- spec/features/crews/characters/index_spec.rb
+++ spec/features/crews/characters/index_spec.rb
@@ -31,5 +31,11 @@ RSpec.describe 'list characters of specific crew', type: :feature do
 
       expect(page).to have_selector(:link_or_button, 'Edit')
     end
+
+    it 'has a delete button near each character' do
+      visit "/characters"
+
+      expect(page).to have_selector(:link_or_button, 'Delete')
+    end
   end
 end
