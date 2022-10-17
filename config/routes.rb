4c134eba553e8cb5e32785a95eedf2ad82f29e2c
Naomi Yocum <naomiyocum24@gmail.com>
2022-10-17 17:04:13 -0600

    fix to create RESTful routes - update new and create to follow crew_characters

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
   get '/', to: 'welcome#index'
-  get '/crews/:id/characters/new', to: 'characters#new'
+
+  get '/crews/:id/characters/new', to: 'crew_characters#new'
+  get '/crews/:id/characters', to:'crew_characters#index'
+  post '/crews/:id/characters', to:'crew_characters#create'
+
   get '/crews', to:'crews#index'
   get '/crews/new', to: 'crews#new'
   get '/crews/:id', to:'crews#show'
   get '/crews/:id/edit', to:'crews#edit'
   
-  get '/crews/:id/characters', to:'crew_characters#index'
+ 
 
   get '/characters/:id/edit', to:'characters#edit'
   get '/characters', to:'characters#index'
@@ -15,7 +19,6 @@ Rails.application.routes.draw do
   post '/crews', to:'crews#create'
   patch '/crews/:id', to: 'crews#update'
 
-  post '/crews/:id/characters', to:'characters#create'
   patch '/characters/:id', to: 'characters#update'
 
   delete 'crews/:id', to:'crews#destroy'
