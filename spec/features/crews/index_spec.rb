03b35b3c6a88f44d409b868cf185d87ffbc366d5
Naomi Yocum <naomiyocum24@gmail.com>
2022-10-18 14:02:14 -0600

    sort crew based on members, extension 1

--- spec/features/crews/index_spec.rb
+++ spec/features/crews/index_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe 'index of crews', type: :feature do
   let!(:crew) {Crew.create!(name: 'Straw Hat Pirates', ranking: 1, ship: 'Thousand Sunny', worst_generation: true)}
   let!(:crew_2) {Crew.create!(name: 'Roger Pirates', ranking: 2, ship: 'Oro Jackson', worst_generation: false)}
+  let!(:silver) {Character.create!(name: 'Silvers Rayleigh', age: 78, devil_fruit_eater: false, epithet: 'Right Hand of PK', bounty: 0, crew_id: crew_2.id)}
 
   describe 'when a user visits the crews index' do
     it 'shows the name of each crew' do
@@ -36,6 +37,16 @@ RSpec.describe 'index of crews', type: :feature do
       visit '/crews'
 
       expect(page).to have_selector(:link_or_button, 'Delete')
+    end
+
+    it 'includes a link to sort crews by members' do
+      visit '/crews'
+
+      click_link 'Sort by Members'
+
+      expect(crew_2.name).to appear_before(crew.name)
+      expect(page).to have_content(crew_2.chars_count)
+      expect(page).to have_content(crew.chars_count)
     end
   end
 end
